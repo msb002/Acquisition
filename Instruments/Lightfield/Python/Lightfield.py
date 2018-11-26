8136839535601bb0d5a966e51bb743413cae0af6
Lee Aspitarte <aspitarte@gmail.com>
2018-11-25 20:54:39 -0800

    get basic python file path generation functionality working
    Former-commit-id: 4aa253f9004ba916a6b0220a7ec192c268900974

--- Instruments/Lightfield/Python/Lightfield.py
+++ Instruments/Lightfield/Python/Lightfield.py
@@ -1,13 +1,15 @@
-import win32com.client  # Python ActiveX Client
-LabVIEW = win32com.client.Dispatch("Labview.Application")
-VI = LabVIEW.getvireference('C:\\Users\\aspit\\Git\\MHDLab\\Common Subvis\\GenerateFilePaths.vi')  # Path to LabVIEW VI
-VI._FlagAsMethod("Call")  # Flag "Call" as Method
-VI.setcontrolvalue('Device Name', "Lightfield") 
-VI.setcontrolvalue('Extension (.tdms)', ".spe")
-VI.setcontrolvalue('DAQmx (F)', False) 
-VI.setcontrolvalue('Logfile? (F)', False) 
+import mhdpy
+import time
 
-# VI.Call()  # Run the VI
+fileinfo = mhdpy.daq.get_fileinfo()
 
-# filepath = VI.getcontrolvalue('Path')  # Get return value
-# print(filepath)  # Print value to console
+filepath = mhdpy.daq.gen_filepath('Lightfield', '.spe')
+
+while(True):
+    fileinfonew = mhdpy.daq.get_fileinfo()
+    if(fileinfonew != fileinfo):
+        fileinfo = fileinfonew
+        filepath = mhdpy.daq.gen_filepath('Lightfield', '.spe')
+        print(filepath)
+    time.sleep(0.1)
+
