8805253b313c1c08353f42fbfcb6990e45cea9a7
msb002 <mstevenbowen@gmail.com>
2018-12-07 14:57:59 -0800

    Fixed the conversions in the hvof visualization
    Former-commit-id: 1ed0469c4021ce389be8e5a783ba36e51178d5e8

--- Visualization/Python Scripts/HVOF_Vis.py
+++ Visualization/Python Scripts/HVOF_Vis.py
@@ -1,9 +1,9 @@
 def HVOF_Calc(arr):
     #Heat capacity of Water (J/KgC)
-    C_w = 4.186
+    C_w = 4185.5
 
     #Density of water (Kg/L)
-    rho_w = 1/1000.0
+    rho_w = 1
 
     #Standard Temperature of Alicats (K)
     Ts = 298.15
@@ -14,26 +14,26 @@ def HVOF_Calc(arr):
     #Density of Oxygen from Ideal Gas Law (g/L)
     rho_O = 32/22.4
 
-    #Density of Kerosene (kg/L)
+    #Density of Kerosene (g/L)
     rho_kero = 0.8148
 
-    #Heat/mass of kerosene (KJ/g)
-    Q_kero = 64.0
+    #Heat/mass of kerosene (J/kg)
+    Q_kero = 64.0E6
 
     #Stoichiometric Ratio 
     stoich = 170.0/592.0
 
     
-    Fl_fuel, Fl_w, T_in, T_out, m_dot_O = arr
+    Fl_fuel, Fl_w, T_in, T_out, m_dot_O = arr #in [lpm, lpm, C, C, SLPM]
     
     #Phi Calculation: mass flow is calculated in g/min
-    m_dot_fuel = Fl_fuel * rho_kero/1000 #1000g/1kg
+    m_dot_fuel = Fl_fuel * rho_kero * 1000 #1000g/1kg
     phi = (m_dot_fuel/m_dot_O)/stoich
     
     #Heat Calculations
     Q_w = (T_out-T_in)*C_w*Fl_w*rho_w * (1/60.0) #1min/60seconds
     
-    Q_fuel = m_dot_fuel*rho_kero * (1/60.0) #1min/60seconds
+    Q_fuel = Fl_fuel*rho_kero* Q_kero * (1/60.0) #1min/60seconds
     
     return [phi, Q_w, Q_fuel]
     
