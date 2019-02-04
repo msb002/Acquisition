b69b4850f82014c3b01276d6af37ee60f94150de
Lee Aspitarte <aspitarte@gmail.com>
2019-02-04 15:28:47 -0800

    add spec file for creating standalone JPsync program

--- FileSync/JPsync.py
+++ FileSync/JPsync.py
@@ -1,12 +1,11 @@
 import os
 import datetime
-import numpy as np
-import pandas as pd
-
-JPpath = 'Z:\\Data\\JP8000\\Reports'
-rawdatapath = 'Z:\\Data\\Raw Data'
-
+# import numpy as np
+# import pandas as pd
 from shutil import copyfile
+
+JPpath = 'C:\\Users\\aspitarl\\Desktop\\JP Data\\Reports'
+rawdatapath = 'C:\\Users\\aspitarl\\OneDrive\\MHD Common Drive\\Data\\Raw Data'
 
 fns = [fn for fn in os.listdir(JPpath) if os.path.isfile(os.path.join(JPpath,fn))]
 
@@ -25,3 +24,23 @@ for fn in fns:
         print('copying ' + outpath)
         copyfile(fp,outpath)
 
+input("Press enter to continue...")
+
+"""
+To build into an excecutable use pyinstaler.
+
+If encountering a recursion error follow instructions here:
+https://stackoverflow.com/questions/38977929/pyinstaller-creating-exe-runtimeerror-maximum-recursion-depth-exceeded-while-ca
+
+to create the .spec file
+pyinstaller JPsync.py -c --onefile 
+
+Then close out and add the lines to spec file
+
+import sys
+sys.setrecursionlimit(5000)
+
+then run 
+
+pyinstaller JPsync.spec   
+"""
